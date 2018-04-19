252bf9ef99ea667813f3eddbb55b6e5d38e2aa12
German Voytekhovich <ueuknjg@gmail.com>
2018-04-20 00:03:43 +0300

    add download img and endless for loop check page

--- main.go
+++ main.go
@@ -1,80 +1,127 @@
 package main
 
 import (
-    "fmt"
-    "github.com/PuerkitoBio/goquery"
-    "log"
     "net/http"
     "time"
+    "log"
+    "github.com/PuerkitoBio/goquery"
+    "fmt"
+    "strings"
+    "os"
+    "io"
 )
 
 func main() {
-    ExampleScrape()
+    var tag string = "sousouman"
+    go grabber(tag)
+    grabber(tag)
 }
-func ExampleScrape() {
-    // Request the HTML page.
-    tr := &http.Transport{
-      MaxIdleConns:       10,
-      IdleConnTimeout:    30 * time.Second,
-      DisableCompression: true,
-    }
-    client := &http.Client{Transport: tr}
-    res, err := client.Get("https://yande.re/post?page=1&tags=sousouman")
-    if err != nil {
-      log.Fatal(err)
-    }
-    defer res.Body.Close()
-    if res.StatusCode != 200 {
-      log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-    }
-  
-    // Load the HTML document
-    doc, err := goquery.NewDocumentFromReader(res.Body)
-    if err != nil {
-      log.Fatal(err)
+
+func grabber(tag string) {
+
+    var checkIsEmpty bool = true
+    page := 1
+
+    for checkIsEmpty {
+
+        var url string = fmt.Sprintf("https://yande.re/post?page=%d&tags=%s", page, tag)
+
+        tr := &http.Transport{
+            MaxIdleConns:       10,
+            IdleConnTimeout:    30 * time.Second,
+            DisableCompression: true,
+        }
+        client := &http.Client{Transport: tr}
+        res, err := client.Get(url)
+
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        defer res.Body.Close()
+        if res.StatusCode != 200 {
+            log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+        }
+
+        doc, err := goquery.NewDocumentFromReader(res.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        checkIsEmpty = doc.Find("#post-list-posts").Is("ul")
+        fmt.Println(checkIsEmpty)
+
+        if checkIsEmpty {
+            doc.Find("#post-list-posts li").Each(func(i int, s *goquery.Selection) {
+                if val, ok := s.Find("a.directlink").Attr("href"); !ok {
+                    fmt.Printf("Nope\n")
+                } else {
+
+                   inputImgName := strings.Split(val, "/")
+                   imgURL := inputImgName[len(inputImgName) - 1]
+                   sourceURL := fmt.Sprintf("D:/test/%s", imgURL)
+
+                   response, e := http.Get(val)
+
+                   if e != nil {
+                      log.Fatal(e)
+                   }
+
+                   defer response.Body.Close()
+
+                   file, err := os.Create(sourceURL)
+
+                   if err != nil {
+                      log.Fatal(err)
+                   }
+
+                   _, err = io.Copy(file, response.Body)
+                   if err != nil {
+                      log.Fatal(err)
+                   }
+
+                   file.Close()
+                }
+            })
+        }
+
+        page++
     }
 
-    //Check list is empty
-    fuck := doc.Find("#post-list-posts").Is("ul")
-    fmt.Println(fuck)
+}
+
+
+func ExampleScrape() {
+
 
 
     //doc.Find("#post-list-posts li").Each(func(i int, s *goquery.Selection) {
-    //  // For each item found, get the band and title
-    //  if val, ok := s.Find("a.directlink").Attr("href"); !ok {
-    //    fmt.Printf("Nope\n")
-    //  } else {
-    //      fmt.Printf("%d - %s\n", i, val)
-
-
-
-
-        //response, e := http.Get(val)
-		//
-        //if e != nil {
-        //    log.Fatal(e)
-        //}
-		//
-        //defer response.Body.Close()
-		//
-        //img := strings.Split(val, ".")
-        //imgType := img[len(img) - 1]
-        //imgName := strings.Split(img[2], "/")
-        //url := fmt.Sprintf("D:/test/%s.%s", imgName[len(imgName) - 1], imgType)
-		//
-        //file, err := os.Create(url)
-		//
-        //if err != nil {
-        //    log.Fatal(err)
-        //}
-		//
-        //_, err = io.Copy(file, response.Body)
-        //if err != nil {
-        //    log.Fatal(err)
-        //}
-        //fmt.Println("Success")
-        //file.Close()
-      //}
+    // if val, ok := s.Find("a.directlink").Attr("href"); !ok {
+    //   fmt.Printf("Nope\n")
+    // } else {
+    //     fmt.Printf("%d - %s\n", i, val)
+	//
+    //    response, e := http.Get(val)
+	//
+    //    if e != nil {
+    //       log.Fatal(e)
+    //    }
+	//
+    //    defer response.Body.Close()
+	//
+    //    file, err := os.Create(url)
+	//
+    //    if err != nil {
+    //       log.Fatal(err)
+    //    }
+	//
+    //    _, err = io.Copy(file, response.Body)
+    //    if err != nil {
+    //       log.Fatal(err)
+    //    }
+    //    fmt.Println("Success")
+    //    file.Close()
+    //  }
     //})
   }
   
