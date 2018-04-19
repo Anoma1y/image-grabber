051ad46ab8397a1b8ecded6be13778a55f41a095
German Voytekhovich <ueuknjg@gmail.com>
2018-04-19 23:18:17 +0300

    initial commit add check empty list

--- main.go
+++ main.go
@@ -6,9 +6,6 @@ import (
     "log"
     "net/http"
     "time"
-    "io"
-    "strings"
-    "os"
 )
 
 func main() {
@@ -22,7 +19,7 @@ func ExampleScrape() {
       DisableCompression: true,
     }
     client := &http.Client{Transport: tr}
-    res, err := client.Get("https://lenta.ru/rubrics/world/")
+    res, err := client.Get("https://yande.re/post?page=1&tags=sousouman")
     if err != nil {
       log.Fatal(err)
     }
@@ -36,39 +33,48 @@ func ExampleScrape() {
     if err != nil {
       log.Fatal(err)
     }
-    fmt.Printf("Start\n")
-    // Find the review items
-    doc.Find(".article a.picture").Each(func(i int, s *goquery.Selection) {
-      // For each item found, get the band and title
-      if val, ok := s.Find("img").Attr("src"); !ok {
-        fmt.Printf("Nope\n")
-      } else {
 
-        response, e := http.Get(val)
-    
-        if e != nil {
-            log.Fatal(e)
-        }
-    
-        defer response.Body.Close()
+    //Check list is empty
+    fuck := doc.Find("#post-list-posts").Is("ul")
+    fmt.Println(fuck)
 
-        img := strings.Split(val, ".")
-        imgType := img[len(img) - 1]
-        imgName := strings.Split(img[2], "/")
-        url := fmt.Sprintf("/home/developer/Projects/%s.%s", imgName[len(imgName) - 1], imgType)
 
-        file, err := os.Create(url)
+    //doc.Find("#post-list-posts li").Each(func(i int, s *goquery.Selection) {
+    //  // For each item found, get the band and title
+    //  if val, ok := s.Find("a.directlink").Attr("href"); !ok {
+    //    fmt.Printf("Nope\n")
+    //  } else {
+    //      fmt.Printf("%d - %s\n", i, val)
 
-        if err != nil {
-            log.Fatal(err)
-        }
 
-        _, err = io.Copy(file, response.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
-        file.Close()
-      }
-    })
+
+
+        //response, e := http.Get(val)
+		//
+        //if e != nil {
+        //    log.Fatal(e)
+        //}
+		//
+        //defer response.Body.Close()
+		//
+        //img := strings.Split(val, ".")
+        //imgType := img[len(img) - 1]
+        //imgName := strings.Split(img[2], "/")
+        //url := fmt.Sprintf("D:/test/%s.%s", imgName[len(imgName) - 1], imgType)
+		//
+        //file, err := os.Create(url)
+		//
+        //if err != nil {
+        //    log.Fatal(err)
+        //}
+		//
+        //_, err = io.Copy(file, response.Body)
+        //if err != nil {
+        //    log.Fatal(err)
+        //}
+        //fmt.Println("Success")
+        //file.Close()
+      //}
+    //})
   }
   
