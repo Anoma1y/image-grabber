e022360d63a4f37e1538680c279994e919e1f5a6
German Voytekhovich <ueuknjg@gmail.com>
2018-04-20 01:25:26 +0300

    add multitags

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "net/http"
-    "time"
     "log"
     "github.com/PuerkitoBio/goquery"
     "fmt"
@@ -12,79 +11,85 @@ import (
 )
 
 func main() {
-    var tag string = "sousouman"
+    var tag = []string{"witch", "dress", "touhou"}
     grabber(tag)
 }
 
-func grabber(tag string) {
+func grabber(tag []string) {
 
     var checkIsEmpty bool = true
     page := 1
 
+    var tags string = ""
+
+    if len(tag) > 1 {
+        for i := 0; i < len(tag); i++  {
+            if i == 0 {
+                tags += tag[i]
+                continue
+            }
+            tags += "+" + tag[i]
+        }
+    } else {
+        tags += tag[0]
+    }
+
     for checkIsEmpty {
 
-        var url string = fmt.Sprintf("https://yande.re/post?page=%d&tags=%s", page, tag)
+       var url string = fmt.Sprintf("https://yande.re/post?page=%d&tags=%s", page, tags)
 
-        tr := &http.Transport{
-            MaxIdleConns:       10,
-            IdleConnTimeout:    30 * time.Second,
-            DisableCompression: true,
-        }
-        client := &http.Client{Transport: tr}
-        res, err := client.Get(url)
+       res, err := http.Get(url)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+       if err != nil {
+           log.Fatal(err)
+       }
 
-        defer res.Body.Close()
-        if res.StatusCode != 200 {
-            log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-        }
+       defer res.Body.Close()
+       if res.StatusCode != 200 {
+           log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+       }
 
-        doc, err := goquery.NewDocumentFromReader(res.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
+       doc, err := goquery.NewDocumentFromReader(res.Body)
+       if err != nil {
+           log.Fatal(err)
+       }
 
-        checkIsEmpty = doc.Find("#post-list-posts").Is("ul")
-        fmt.Println(checkIsEmpty)
+       checkIsEmpty = doc.Find("#post-list-posts").Is("ul")
 
-        if checkIsEmpty {
-            doc.Find("#post-list-posts li").Each(func(i int, s *goquery.Selection) {
-                if val, ok := s.Find("a.directlink").Attr("href"); !ok {
-                    fmt.Printf("Nope\n")
-                } else {
+       if checkIsEmpty {
+          doc.Find("#post-list-posts li").Each(func(i int, s *goquery.Selection) {
+              if val, ok := s.Find("a.directlink").Attr("href"); !ok {
+                  fmt.Printf("Nope\n")
+              } else {
+                 inputImgName := strings.Split(val, "/")
+                 imgURL := inputImgName[len(inputImgName) - 1]
+                 sourceURL := fmt.Sprintf("D:/test/%s", imgURL)
 
-                   inputImgName := strings.Split(val, "/")
-                   imgURL := inputImgName[len(inputImgName) - 1]
-                   sourceURL := fmt.Sprintf("D:/test/%s", imgURL)
+                 response, e := http.Get(val)
 
-                   response, e := http.Get(val)
+                 if e != nil {
+                  log.Fatal(e)
+                 }
 
-                   if e != nil {
-                      log.Fatal(e)
-                   }
+                 defer response.Body.Close()
 
-                   defer response.Body.Close()
+                 file, err := os.Create(sourceURL)
 
-                   file, err := os.Create(sourceURL)
+                 if err != nil {
+                  log.Fatal(err)
+                 }
 
-                   if err != nil {
-                      log.Fatal(err)
-                   }
+                 _, err = io.Copy(file, response.Body)
+                 if err != nil {
+                  log.Fatal(err)
+                 }
 
-                   _, err = io.Copy(file, response.Body)
-                   if err != nil {
-                      log.Fatal(err)
-                   }
+                 file.Close()
+              }
+          })
+       }
 
-                   file.Close()
-                }
-            })
-        }
-
-        page++
+       page++
     }
 
 }
